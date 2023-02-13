fb469d4615986a8831d0deca10c8abb514fabd0f
Imran Fazilov <ifazilov3584@bths.edu>
2023-02-13 12:27:22 -0500

    lab done

--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Sort {
+import java.util.ArrayList;
+
+public class Sort
+{
+    // Part A.  Which Sort is Which?
+    public static void selectionSort(int[] elements)
+    {
+        int counter = 0;
+        for (int j = 0; j < elements.length - 1; j++)
+        {
+            int minIndex = j;
+            for (int k = j + 1; k < elements.length; k++)
+            {
+                if (elements[k] < elements[minIndex])
+                {
+                    minIndex = k;
+                }
+                counter++;
+            }
+            int temp = elements[j];
+            elements[j] = elements[minIndex];
+            elements[minIndex] = temp;
+        }
+        System.out.println(counter);
+    }
+
+    public static void insertionSort(int[] elements)
+    {
+        int counter = 0;
+        for (int j = 1; j < elements.length; j++)
+        {
+            int temp = elements[j];
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+            {
+                elements[possibleIndex] = elements[possibleIndex - 1];
+                possibleIndex--;
+                counter++;
+            }
+            elements[possibleIndex] = temp;
+        }
+        System.out.println(counter);
+    }
+
+    // Part C.  Sorting a 1000-word list!
+    public static void selectionSortWordList(ArrayList<String> words)
+    {
+        for (int j = 0; j < words.size() - 1; j++)
+        {
+            int minIndex = j;
+            for (int k = j + 1; k < words.size(); k++)
+            {
+                if (words.get(k).compareTo(words.get(minIndex)) < 0)
+                {
+                    minIndex = k;
+                }
+            }
+            String temp = words.set(j, words.get(minIndex));
+            words.set(minIndex, temp);
+        }
+    }
+
+    public static void insertionSortWordList(ArrayList<String> words)
+    {
+        for (int j = 1; j < words.size(); j++)
+        {
+            String temp = words.get(j);
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0)
+            {
+                words.set(possibleIndex, words.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            words.set(possibleIndex, temp);
+        }
+    }
 }
